a0ec841c75eedcd9fe30c143ae15a873a02abc9a
Dimkouv <cse42730@cs.uoi.gr>
2018-02-20 04:36:14 +0200

    keep classifiers in memory for improved speed

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS, cross_origin
-from test import get_predictions
+import pickle
+from utilities import clean
 
 application = Flask(__name__)
 CORS(application)
 application.debug = True
+
+columns = ["toxic", "severe_toxic", "obscene",
+            "threat", "insult", "identity_hate"]
+
+classifiers = {}
+for c in columns:
+    filename = "./data/%s_classifier.sav" % (c)
+    classifiers[c] = pickle.load(open(filename, "rb"))
+
+vectorizer = pickle.load(open("./data/vectorizer.sav", "rb"))
+
 
 @application.route("/")
 def hello():
@@ -15,7 +27,11 @@ def hello():
 
 @application.route("/predict/<msg>")
 def predict(msg):
-    return jsonify(get_predictions(msg))
+    predictions = {}
+    test_vector = vectorizer.transform([clean(msg)])
+    for c in columns:
+        predictions[c] = classifiers[c].predict_proba(test_vector)[:,-1][0]
+    return jsonify(predictions)
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0")
